fae79f48b37110d49e41d5ab90af448d23a5dd9d
Matt Lehrer <lehrerm@gmail.com>
2021-03-15 10:56:37 -0700

    wip

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,9 +1,7 @@
 <script>
-	import Nav from '$components/Nav.svelte';
 	import '../style.css';
 </script>
 
-<Nav />
 <main>
 	<slot />
 </main>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,25 @@
 <script>
-	import Counter from '$components/Counter.svelte';
+	import { sun } from '@modern-dev/daylight';
+
+	const date = new Date();
+	const lat = 47.62965;
+	const long = -122.321281;
+
+	console.log(sun.getTimes(date, lat, long).astronomicalDawn);
 </script>
 
-<main class="p-4 mx-auto text-center">
-	<h1
-		class="text-red-500 uppercase text-6xl font-thin my-16 mx-auto max-w-xs sm:max-w-none"
-	>
-		Hello world!
-	</h1>
-
-	<Counter />
-	<p class="max-w-xs my-8 mx-auto sm:max-w-none">
-		Visit the <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-		apps.
-	</p>
+<main class="grid grid-cols-1 w-full min-h-full">
+	<hr />
 </main>
+
+<style>
+	main {
+		grid-template-rows: repeat(24, minmax(0, 1fr));
+		background: linear-gradient(
+			180deg,
+			rgba(0, 0, 0, 1) 0%,
+			rgba(9, 9, 121, 1) 50%,
+			rgba(5, 17, 201, 1) 100%
+		);
+	}
+</style>
